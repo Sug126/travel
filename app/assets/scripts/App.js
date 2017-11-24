beaf635478a0e1b84c32d1e1f601d6f34847552f
Sugandha <raosugandha12@gmail.com>
2017-11-24 21:14:11 +0000

    completed revealonscroll behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,4 +3,11 @@
 
 import MobileMenu from './modules/MobileMenu';
 
+import RevealOnScroll from './modules/RevealOnScroll';
+
+import $ from 'jquery';
+
 var mobileMenu = new MobileMenu();
+
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonials"), "60%");
