53349e596b5d6637f6c37cca012f8f422e15fcd6
Sugandha <raosugandha12@gmail.com>
2017-11-27 16:55:15 +0000

    modal completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,9 +5,16 @@ import MobileMenu from './modules/MobileMenu';
 
 import RevealOnScroll from './modules/RevealOnScroll';
 
+import StickyHeader from './modules/StickyHeader';
+
+import Modal from './modules/Modal';
+
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonials"), "60%");
+
+var stickyHeader = new StickyHeader();
+var modal = new Modal();
