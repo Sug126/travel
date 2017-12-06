c43a997873485052db705200a19e08c7119483d6
Sugandha <raosugandha12@gmail.com>
2017-12-06 13:36:40 +0000

    complted buil task

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,16 @@
-const path: require('path');
-
 module.exports = {
- entry: {
+  entry: {
     App: "./app/assets/scripts/App.js",
     Vendor: "./app/assets/scripts/Vendor.js"
   },
   output: {
-  path:path.resolve(__dirname, "./app/temp/scripts"),
+    path: "./app/temp/scripts",
     filename: "[name].js"
   },
   module: {
     loaders: [
       {
-        loader: 'babel-loader',
+        loader: 'babel',
         query: {
           presets: ['es2015']
         },
